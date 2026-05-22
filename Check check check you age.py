591a918d459fada488d8ed1f78b4de21661b7458
Nnamdimatthews <adaezeekoh.jc@gmail.com>
2026-05-22 14:44:09 +0100

    Add files via upload

---
+++ Check check check you age.py
@@ -0,0 +1,104 @@
+print("Welcome to the Age Checker! Let's find out if you are old enough to do certain activities.")
+print("Welcome to ze Gamer Ager! Let's see if you're old enough to play these games on poki!")
+age = int(input("Please enter your age: "))
+if age >= 20:
+    print("Whooh! You're old enough to play all the games, try more mature games like Call of Duty, Grand Theft Auto and Minecraft!")
+elif age >= 8:
+    print("You are old enough to play fortride: open world. here is the link to fortride: https://poki.com/en/g/fortride-open-world")
+elif age >= 6:
+    print("You are old enough to play Goal Gang. here is the link to Goal Gang: https://poki.com/en/g/goal-gang")
+elif age >= 5:
+    print("You are old enough to play Red Ball. here is the link to Red Ball: https://poki.com/en/g/red-ball-4?msockid=1f04f47747a265f524c1e7cd4669649e")
+elif age >= 4:
+    print("You are old enough to play Save Dogogo. here is the link to Save Dogogo: https://poki.com/en/g/save-dogogo?msockid=1f04f47747a265f524c1e7cd4669649e")
+elif age >= 3:
+    print("You are old enough to play Logic Egg Puzzle. Here is the link to Logic Egg Puzzle: https://poki.com/en/g/draw-to-smash-logic-puzzle")
+else:
+    print("Sorry, you are not old enough to play any of the games. Please come back when you are older!")
+
+print ("\nThank you for using the Gamer Ager! Next Please!")
+
+
+print("\nWelcome to the Grade Checker! Let's find out the grade you are in.")
+grade = int(input("Please enter your age: "))
+if grade >= 11:
+    print("You are in middle school!")
+elif grade >= 10:
+    print("You are in grade 6!")
+elif grade >= 9:
+    print("You are in grade 5!")
+elif grade >= 8:
+    print("You are in grade 4!")
+elif grade >= 7:
+    print("You are in grade 3!")
+elif grade >= 6:
+    print("You are in grade 2!")
+elif grade >= 5:
+    print("You are in grade 1!")
+else:
+    print("You are not in school yet! Please come back when you are older!")
+
+print("\nThank you for using the Grade Age Checker! Next Please!")
+
+print("\nWelcome to the Age Sports Checker! Let's find out if you are old enough to do certain sports.")
+
+if age >= 10-12:
+    print("You are old enough to play basketball!")
+elif age >= 7-8:
+    print("You are old enough to play soccer!")
+elif age >= 5-6:
+    print("You are old enough to play baseball!")
+elif age >= 3-4:
+    print("You are old enough to play tee-ball!")
+else:
+    print("Sorry, you are not old enough to play any of the sports. Please come back when you are older!")
+
+print("\nThank you for using the Age Sports Checker! Next Please!")
+print("\nWelcome to the Age Movie Checker! Let's find out if you are old enough to watch certain movies.")
+
+if age >= 10:
+    print("You are old enough to watch The Incredibles 2, The Iron Giant and Zootopia!")
+elif age >= 8:
+   print("You are old enough to watch The Lion King, Sonic the Hedgehog 3 and Toy Story!")
+elif age >= 6:
+    print("You are old enough to watch Finding Nemo, The Secret Life of Pets and Despicable Me!")
+elif age >= 4:
+    print("You are old enough to watch Frozen, Moana and The Boss Baby!")
+elif age >= 2:
+    print("You are old enough to watch Cars, Minions and The Lego Movie!")
+else:
+    print("Sorry, you are not old enough to watch any of the movies. Please come back when you are older!")
+print("\nThank you for using the Age Movie Checker! Next Please!")
+
+print("\nWelcome to the Age Driver! Let's find out if you are old enough to drive.")
+if age >= 16:
+    print("You are old enough to drive! Please make sure to follow all traffic laws and drive safely!")
+else:
+    print("Sorry, you are not old enough to drive. Please come back when you are older!")
+print("\nThank you for using the Age Driver Checker! Next Please!")
+
+print("\nWelcome to the Age Voter! Let's find out if you are old enough to vote.")
+if age >= 18:
+    print("You are old enough to vote! Please make sure to register to vote and exercise your right to vote!")
+elif age >= 16:
+    print("You are old enough to pre-register to vote! Please make sure to pre-register to vote and exercise your right to vote when you turn 18!")
+elif age >= 14:
+    print("You are old enough to learn about voting and the political process! Please make sure to educate yourself about voting and the political process so you can be an informed voter when you turn 18!")
+elif age >= 12:
+    print("You are old enough to learn about the importance of voting and how it impacts our society! Please make sure to educate yourself about the importance of voting and how it impacts our society so you can be an informed voter when you turn 18!")
+elif age >= 10:
+    print("You are old enough to learn about the history of voting and how it has evolved over time! Please make sure to educate yourself about the history of voting and how it has evolved over time so you can be an informed voter when you turn 18!")
+elif age >= 8:
+    print("You are old enough to learn about the different political parties and their platforms! Please make sure to educate yourself about the different political parties and their platforms so you can be an informed voter when you turn 18!")
+elif age >= 7:
+    print("You are old enough to learn about the importance of civic engagement and how it impacts our society! Please make sure to educate yourself about the importance of civic engagement and how it impacts our society so you can be an informed voter when you turn 18!")
+else:
+    print("Sorry, you are not old enough to vote. Please come back when you are older!")
+
+print("\nThank you for using the Age Voter Checker! Next Please!")
+print("\nWelcome to the Age Snacker! Let's find out if you are old enough to take snacks!")
+if age >= 5:
+    print("You are old enough to take snacks! Please make sure to eat healthy and enjoy your snacks in moderation!")
+else:
+    print("Sorry, you are not old enough to take snacks. Please come back when you are older!")
+print("\nThank you for using the Age Snacker Checker! Great job! Thank you for using the checking your age for these activities!")
